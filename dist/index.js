7f672bf39fb8d7288aa7987398416eea6a656f18
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-05 00:13:03 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+// Example 1 ------------------
+// const firstName="Emre";
+// const favoritedNumber=12;
+// Fonksiyonun parametresinin type=string
+// function printName(name:string){
+//     console.log(name);
+// }
+// Eger Burda number degişkenini gönderseydik ben senden string parametre bekliyordum sen bana number verdin diye hata patlatabilir
+// printName(firstName);
+// Example 2 ------------------
+let firstName = "Emre";
+let number = 16;
+let isStudent = true;
+// firstName=14;
+firstName = "Sipahi Hoca(:";
+// number="12" > 12;
+const favoritedNumberMessage = number + " :";
+console.log(favoritedNumberMessage);
+const square = (num) => {
+    return num * 5;
+};
+// console.log(square("Emre"))
+console.log(square(12));
+console.log(square(4.67));
+export {};
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+// Example 1 ------------------
+
+// const firstName="Emre";
+// const favoritedNumber=12;
+
+// Fonksiyonun parametresinin type=string
+// function printName(name:string){
+//     console.log(name);
+// }
+
+// Eger Burda number degişkenini gönderseydik ben senden string parametre bekliyordum sen bana number verdin diye hata patlatabilir
+// printName(firstName);
+
+
+// Example 2 ------------------
+let firstName ="Emre";
+let number=16;
+let isStudent=true;
+
+// firstName=14;
+firstName="Sipahi Hoca(:";
+// number="12" > 12;
+
+const favoritedNumberMessage=number+" :";
+console.log(favoritedNumberMessage)
+
+const square=(num: number)=>{
+    return num*5;
+}
+
+// console.log(square("Emre"))
+console.log(square(12));
+
+console.log(square(4.67))
