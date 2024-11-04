8e25db207402c4cbc7d3a26cfd6f4fa6b8b42404
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-05 00:40:55 +0300

    array and object

--- dist/index.js
+++ dist/index.js
@@ -7,20 +7,47 @@
 // }
 // Eger Burda number degişkenini gönderseydik ben senden string parametre bekliyordum sen bana number verdin diye hata patlatabilir
 // printName(firstName);
-// Example 2 ------------------
-let firstName = "Emre";
-let number = 16;
-let isStudent = true;
-// firstName=14;
-firstName = "Sipahi Hoca(:";
-// number="12" > 12;
-const favoritedNumberMessage = number + " :";
-console.log(favoritedNumberMessage);
-const square = (num) => {
-    return num * 5;
+// Example 2 --------------------
+// let firstName ="Emre";
+// let number=16;
+// let isStudent=true;
+// // firstName=14;
+// firstName="Sipahi Hoca(:";
+// // number="12" > 12;
+// const favoritedNumberMessage=number+" :";
+// console.log(favoritedNumberMessage)
+// const square=(num: number)=>{
+//     return num*5;
+// }
+// // console.log(square("Emre"))
+// console.log(square(12));
+// console.log(square(4.67))
+// Example 3 ------------------- Arrays
+const names = ["Vickie", "Lena", "Abigail", "Bobbie"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+names.push("Hildred");
+const namesAndNumbers = [...names, ...numbers];
+console.log(namesAndNumbers);
+const renameName = "Emre";
+namesAndNumbers[2] = renameName;
+console.log("İsim Degiştirildi", namesAndNumbers);
+// Example 4 ---------------- Object
+let user = {
+    firstName: "Emre",
+    lastName: "Seferoğlu",
+    age: 16,
+    birthDate: new Date(2008, 3, 22),
 };
-// console.log(square("Emre"))
-console.log(square(12));
-console.log(square(4.67));
+console.log(user.birthDate);
+user.firstName = "Ahmet";
+// user.firstName=1;!!! 
+user.birthDate = new Date(2008, 2, 4);
+console.log(user.birthDate);
+user = {
+    lastName: "Seferoğlu",
+    age: 16,
+    birthDate: new Date(2008, 3, 22),
+    firstName: "Ahmet",
+};
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,52 @@
 // printName(firstName);
 
 
-// Example 2 ------------------
-let firstName ="Emre";
-let number=16;
-let isStudent=true;
+// Example 2 --------------------
+// let firstName ="Emre";
+// let number=16;
+// let isStudent=true;
+// // firstName=14;
+// firstName="Sipahi Hoca(:";
+// // number="12" > 12;
+// const favoritedNumberMessage=number+" :";
+// console.log(favoritedNumberMessage)
+// const square=(num: number)=>{
+//     return num*5;
+// }
+// // console.log(square("Emre"))
+// console.log(square(12));
+// console.log(square(4.67))
 
-// firstName=14;
-firstName="Sipahi Hoca(:";
-// number="12" > 12;
 
-const favoritedNumberMessage=number+" :";
-console.log(favoritedNumberMessage)
+// Example 3 ------------------- Arrays
+const names=["Vickie","Lena","Abigail","Bobbie"];
+const numbers=[1,2,3,4,5,6,7,8,9,10];
+names.push("Hildred");
 
-const square=(num: number)=>{
-    return num*5;
+const namesAndNumbers=[...names,...numbers];
+console.log(namesAndNumbers)
+const renameName="Emre";
+
+namesAndNumbers[2]=renameName;
+console.log("İsim Degiştirildi" ,namesAndNumbers)
+
+
+// Example 4 ---------------- Object
+let user={
+    firstName:"Emre",
+    lastName:"Seferoğlu",
+    age:16,
+    birthDate:new Date(2008,3,22),
 }
+console.log(user.birthDate);
+user.firstName="Ahmet";
+// user.firstName=1;!!! 
+user.birthDate=new Date(2008,2,4);
+console.log(user.birthDate);
 
-// console.log(square("Emre"))
-console.log(square(12));
-
-console.log(square(4.67))
+user={
+    lastName:"Seferoğlu",
+    age:16,
+    birthDate:new Date(2008,3,22),
+    firstName:"Ahmet",
+}
