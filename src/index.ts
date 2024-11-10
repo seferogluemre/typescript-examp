84488d3b62ab18f6dc4b44961d0b499d320c2de8
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-10 12:09:52 +0300

    ts class

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@
 // Eger Burda number degişkenini gönderseydik ben senden string parametre bekliyordum sen bana number verdin diye hata patlatabilir
 // printName(firstName);
 
-
 // Example 2 --------------------
 // let firstName ="Emre";
 // let number=16;
@@ -28,121 +27,114 @@
 // console.log(square(12));
 // console.log(square(4.67))
 
-
 // Example 3 ------------------- Arrays
-const names=["Vickie","Lena","Abigail","Bobbie"];
-const numbers=[1,2,3,4,5,6,7,8,9,10];
+const names = ["Vickie", "Lena", "Abigail", "Bobbie"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 names.push("Hildred");
 
-const namesAndNumbers=[...names,...numbers];
-console.log(namesAndNumbers)
-const renameName="Emre";
+const namesAndNumbers = [...names, ...numbers];
+console.log(namesAndNumbers);
+const renameName = "Emre";
 
-namesAndNumbers[2]=renameName;
-console.log("İsim Degiştirildi" ,namesAndNumbers)
-
+namesAndNumbers[2] = renameName;
+console.log("İsim Degiştirildi", namesAndNumbers);
 
 // Example 4 ---------------- Object
-let user={
-    firstName:"Emre",
-    lastName:"Seferoğlu",
-    age:16,
-    birthDate:new Date(2008,3,22),
-}
+let user = {
+  firstName: "Emre",
+  lastName: "Seferoğlu",
+  age: 16,
+  birthDate: new Date(2008, 3, 22),
+};
 console.log(user.birthDate);
-user.firstName="Ahmet";
-// user.firstName=1;!!! 
-user.birthDate=new Date(2008,2,4);
+user.firstName = "Ahmet";
+// user.firstName=1;!!!
+user.birthDate = new Date(2008, 2, 4);
 console.log(user.birthDate);
 
-user={
-    lastName:"Seferoğlu",
-    age:16,
-    birthDate:new Date(2008,3,22),
-    firstName:"Ahmet",
-}
-
+user = {
+  lastName: "Seferoğlu",
+  age: 16,
+  birthDate: new Date(2008, 3, 22),
+  firstName: "Ahmet",
+};
 
 // Example 5 ---------------- Explicit Types
-let firstName:string;
-let age:number;
-let isBanned:boolean;
+let firstName: string;
+let age: number;
+let isBanned: boolean;
 
 // age="emre";
-age=12;
+age = 12;
 // age=true;
 
 // isBanned="aktif";
-isBanned=true;
+isBanned = true;
 
-firstName= "emre";
+firstName = "emre";
 // firstName=12;
 
-let users:string []=[];
+let users: string[] = [];
 // users.push(12);
 users.push("emre");
 
-let usersAndNumbers: (string | number)[]=[];
+let usersAndNumbers: (string | number)[] = [];
 usersAndNumbers.push("Ahmet");
 usersAndNumbers.push(1);
 // usersAndNumbers.push(true);
 
-let userOrNumbers:string | number;
-userOrNumbers="Emre";
-userOrNumbers=12;
+let userOrNumbers: string | number;
+userOrNumbers = "Emre";
+userOrNumbers = 12;
 // userOrNumbers=true;
 
+let UserOne: object;
+UserOne = { firstName: "Emre", age: 12, email: "ynsemr53@gmail.com" };
+// UserOne=["emre"]; =>> sakıncalı durum object ile tanımladıgımızda array bile tanımlanabilir
 
-let UserOne:object;
-UserOne={firstName:"Emre",age:12,email:"ynsemr53@gmail.com"};
-// UserOne=["emre"]; =>> sakıncalı durum object ile tanımladıgımızda array bile tanımlanabilir 
+let User2: {
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  age: number;
+  // address?:string,Kullanıcı Adresi girmediginde artık hata vermiyor çünkü ? ile boşda olabilir doluda olabilir demiş olduk
+  isBanned: boolean;
+};
 
+console.log(User2);
 
-let User2:{
-    firstName:string,
-    lastName:string,
-    birthDate:Date,
-    age:number,
-    // address?:string,Kullanıcı Adresi girmediginde artık hata vermiyor çünkü ? ile boşda olabilir doluda olabilir demiş olduk
-    isBanned:boolean,
-}
-
-console.log(User2)
-
-User2={
-    firstName:"Emre",
-    lastName:"Seferoglu",
-    // address:"RİZE",
-    birthDate:new Date(2008,3,22),
-    age:16,
-    isBanned:true,
-}
+User2 = {
+  firstName: "Emre",
+  lastName: "Seferoglu",
+  // address:"RİZE",
+  birthDate: new Date(2008, 3, 22),
+  age: 16,
+  isBanned: true,
+};
 
 // Any ---------------------------
 
-let name:any;
+let name: any;
 
-name="Emre";
-name=true;
-name=16;
-name=["Emre","Ahmet","Mehmet"];
+name = "Emre";
+name = true;
+name = 16;
+name = ["Emre", "Ahmet", "Mehmet"];
 
-let mixed: any[]=[];
+let mixed: any[] = [];
 mixed.push("Mehmet");
 mixed.push(12);
-mixed.push(true)
+mixed.push(true);
 
 // let anyUser:{name:any,age:any,isloggedIn:any};
 // anyUser={name:"Emre",age:25,isLoggedIn:false};
 // anyUser={name:"Ahmet",age:16,isLoggedIn:"Hayır"};
 
-
-
 // Functions ---------------------------------
 
-let greet=()=>{
-    console.log("Sitemize hoşgeldiniz")
-}
+let greet = () => {
+  console.log("Sitemize hoşgeldiniz");
+};
 // greet="Merhaba" !!!!!!
 
 // greet=()=>{
@@ -158,60 +150,97 @@ let greet=()=>{
 // sum(10,2);
 // sum(12,5,12);
 
-
-const sum=(num1:number,num2:number):number=>{
-    return num1+num2
-}
-let result=sum(12,5)
-
-
+const sum = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+let result = sum(12, 5);
 
 // Type Aliases -----------------------------------
 
-
 // Aslında Referans alma işlemine benzer tekrarlamayı önler ve sadece type'ları birkere tanımlar istedigimiz yerde istedigimiz kadar kullanırız
 
-type StringOrNumber=string|number;
-type User={
-    id:StringOrNumber,firstName:string,lastName:string
-}
-const showUserInfo=(id:StringOrNumber,userName:string)=>{
-    console.log(`${userName} kullanıcısının Id'si şudur ${id}`)
-}
+type StringOrNumber = string | number;
+type User = {
+  id: StringOrNumber;
+  firstName: string;
+  lastName: string;
+};
+const showUserInfo = (id: StringOrNumber, userName: string) => {
+  console.log(`${userName} kullanıcısının Id'si şudur ${id}`);
+};
 
-const sendWelcmMessage=(user:User)=>{
-    console.log(`Hoşgeldin ${user.firstName} ${user.lastName}!`)
-}
+const sendWelcmMessage = (user: User) => {
+  console.log(`Hoşgeldin ${user.firstName} ${user.lastName}!`);
+};
 
-const sendGoodBye=(user:User)=>{
-    console.log(`Tekrar bekleriz,${firstName}`)
-}
+const sendGoodBye = (user: User) => {
+  console.log(`Tekrar bekleriz,${firstName}`);
+};
 
 // TYPE CASTİNG ----------------------------------------
 
-const linkEl=document.querySelector("a")!;
-console.log(linkEl)
+const linkEl = document.querySelector("a")!;
+console.log(linkEl);
 
 // formda htmlformelement gözüküyor sebepi zaten direk bir html elemanını aldıgımız için bunun htmlformElement oldugunu anladı ve üzerine geldigimizde öyle yazdı
-const form=document.querySelector("form");
+const form = document.querySelector("form");
 
 // Burda ise direk Element olarak gözükmesininin sebebi class ile belirttigimiz için ve bu class her Elementte olabilecegi için Element yazmış oldu Genel elemanları gördügü içinde kaynaklanıyor
 
 // const formFromClass=document.querySelector(".signup-form") as HTMLFormElement;
 
-// Üsste ise as htmlformelemnt deme sebebimiz aslında sol kısımda sen onu yanlış biliyorsun dogrusu bu gibisinden düşünebiliriz 
-// 
+// Üsste ise as htmlformelemnt deme sebebimiz aslında sol kısımda sen onu yanlış biliyorsun dogrusu bu gibisinden düşünebiliriz
+//
 
 // Burda Varsayalımki herhangi kısımda string oldugundan eminsek ve alttaki gibi hata veriyorsa şunu yapıcaz
 // const formFromClass=document.querySelector(".signup-form") as string; Conversion of type 'Element' to type 'string' may be a mistake because neither type sufficiently overlaps with the other Böyle bi hata ile karşılaşırız burda ilk başta unknown yani bilinmiyor diye belirtip daha string ataması yapabiliriz
 
-const formFromClass=document.querySelector(".signup-form") as unknown as string;
+const formFromClass = document.querySelector(
+  ".signup-form"
+) as unknown as string;
 
+// TypeScript Class -------------------------------------------
 
+class Product {
+  name: string;
+  price: number;
 
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+}
 
+type UserParams = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  phoneNumber: string;
+  email: string;
+};
 
+class UserController {
+  private firstName: string;
+  private lastName: string;
+  private age: number;
+  private phoneNumber: string;
+  private email: string;
+  private cart: Product[];
 
+  constructor({ firstName, lastName, age, phoneNumber, email }: UserParams) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
 
+  addToCart(product) {
+    this.cart.push(product);
+  }
 
-
+  removeFromCart(name: (typeof Product)["name"]) {
+    const otherProduct = this.cart.filter((product) => product.name !== name);
+    this.cart = otherProduct;
+  }
+}
